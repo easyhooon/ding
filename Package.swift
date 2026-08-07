9d09cc715448178a049c724fe7a7e65e9857a00e
easyhooon <mraz3068@gmail.com>
2026-08-07 17:35:42 +0900

    chore: prepare SwiftPM 0.5.0

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let dingVersion = "0.5.0"
+let dingChecksum = "d5230e37d7ba8caaf62706f01f91bbd2d1016a3422088b765586ae06bc1b9d84"
+
+let package = Package(
+    name: "Ding",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "Ding",
+            targets: ["Ding"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Ding",
+            url: "https://github.com/easyhooon/ding/releases/download/\(dingVersion)/Ding.xcframework.zip",
+            checksum: dingChecksum
+        ),
+    ]
+)
